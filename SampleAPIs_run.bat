8cc4eda3f472c00b774ec6e8c324da851a11326c
GeekTech1234 <bhavikparmargithub@gmail.com>
2026-01-05 13:00:21 +0530

    changes to bat file for Sample APIs

--- SampleAPIs_run.bat
+++ SampleAPIs_run.bat
@@ -2,14 +2,14 @@
 setlocal
 
 REM Get timestamp using PowerShell (no WMIC, no delayed expansion)
-for /f %%i in ('powershell -command "Get-Date -Format yyyyMMdd_HHmmss"') do set ts=%%i
+for /f %%i in ('powershell -command "Get-Date -Format yyyyMMdd_HHmmss_fff"') do set ts=%%i
 
 echo Running Newman with timestamp: %ts%
 
 REM Run Newman
 newman run SampleAPIs.postman_collection.json ^
 -d BulkUser_DataDrivenTest.csv -n 2 -r htmlextra ^
---reporter-htmlextra-export "C:\Users\Public\Documents\API_Results\SampleAPIs\SampleAPI_Report_%ts%.html" ^
+--reporter-htmlextra-export "C:\Users\Public\Documents\API_Results\SampleAPIs\SampleAPI_Report_CSV_%ts%.html" ^
 --reporter-htmlextra-browserTitle "API Test Report" ^
 --reporter-htmlextra-title "Sample APIs Test Result" ^
 --reporter-htmlextra-titleSize 3 ^
@@ -18,14 +18,14 @@ newman run SampleAPIs.postman_collection.json ^
 
 
 REM Get timestamp using PowerShell (no WMIC, no delayed expansion)
-for /f %%i in ('powershell -command "Get-Date -Format yyyyMMdd_HHmmss"') do set ts=%%i
+for /f %%i in ('powershell -command "Get-Date -Format yyyyMMdd_HHmmss_fff"') do set ts=%%i
 
 echo Running Newman with timestamp: %ts%
 
 REM Run Newman
 newman run SampleAPIs.postman_collection.json ^
 -d BulkUser_DataDrivenTest.json -n 2 -r htmlextra ^
---reporter-htmlextra-export "C:\Users\Public\Documents\API_Results\SampleAPIs\SampleAPI_Report_%ts%.html" ^
+--reporter-htmlextra-export "C:\Users\Public\Documents\API_Results\SampleAPIs\SampleAPI_Report_JSON_%ts%.html" ^
 --reporter-htmlextra-browserTitle "API Test Report" ^
 --reporter-htmlextra-title "Sample APIs Test Result" ^
 --reporter-htmlextra-titleSize 3 ^
