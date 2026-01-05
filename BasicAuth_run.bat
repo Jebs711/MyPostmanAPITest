6187e41224b622796259495cd8956dd391b42706
GeekTech1234 <bhavikparmargithub@gmail.com>
2026-01-05 12:18:15 +0530

    made changes to bat file

--- BasicAuth_run.bat
+++ BasicAuth_run.bat
@@ -1,16 +1,17 @@
 @echo off
-setlocal EnableDelayedExpansion
+setlocal
 
-REM Get timestamp using PowerShell (WMIC-free)
+REM Get timestamp using PowerShell (no WMIC, no delayed expansion)
 for /f %%i in ('powershell -command "Get-Date -Format yyyyMMdd_HHmmss"') do set ts=%%i
 
-echo Running Newman with timestamp: !ts!
+echo Running Newman with timestamp: %ts%
 
 REM Run Newman
 newman run BasicAuth_Razorpay.postman_collection.json -r htmlextra ^
---reporter-htmlextra-export C:\Users\Public\Documents\API_Results\BasicAuthAPIs\BasicAuthAPI_Report_!ts!.html ^
+--reporter-htmlextra-export "C:\Users\Public\Documents\API_Results\BasicAuthAPIs\BasicAuthAPI_Report_%ts%.html" ^
 --reporter-htmlextra-browserTitle "API Test Report" ^
 --reporter-htmlextra-title "BasicAuthAPIs Test Result" ^
 --reporter-htmlextra-titleSize 3 ^
 --reporter-htmlextra-logs ^
 --suppress-exit-code
+
