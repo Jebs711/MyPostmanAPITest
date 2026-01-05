66031d60c7b576688118b4db4aeedf5a99875ab3
GeekTech1234 <bhavikparmargithub@gmail.com>
2026-01-05 13:40:17 +0530

    added missing env.json in bat file

--- PetStoreAPI_run.bat
+++ PetStoreAPI_run.bat
@@ -7,7 +7,7 @@ for /f %%i in ('powershell -command "Get-Date -Format yyyyMMdd_HHmmss"') do set 
 echo Running Newman with timestamp: %ts%
 
 REM Run Newman
-newman run PetStoreAPIs.postman_collection.json -r htmlextra ^
+newman run PetStoreAPIs.postman_collection.json -e Swagger_PetStore.postman_environment.json -r htmlextra ^
 --reporter-htmlextra-export "C:\Users\Public\Documents\API_Results\SwaggerPetStoreAPIs\PetStore_API_Report_%ts%.html" ^
 --reporter-htmlextra-browserTitle "API Test Report" ^
 --reporter-htmlextra-title "PetStoreAPIs Test Result" ^
